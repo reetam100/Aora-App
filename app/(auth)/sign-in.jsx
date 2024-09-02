69fa36c4ba46878986833c2f5e9a6032dbb0740c
Reetam101 <reetamraj2@gmail.com>
2024-09-02 10:25:56 +0530

    setting up appwrite

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -10,6 +10,7 @@ import { Link } from "expo-router";
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
+    password: "",
   });
 
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -19,13 +20,13 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full  px-4 my-6">
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
           <Image
             source={images.logo}
             resizeMode="contain"
             className="w-[115px] h-[35px]"
           />
-          <Text className="text-2xl text-white text-semibold mt-10 font-semibold">
+          <Text className="text-2xl text-white text-semibold mt-10 font-semibold mb-5">
             Log in to Aura
           </Text>
           <FormField
--- components/FormField.jsx
+++ components/FormField.jsx
@@ -14,7 +14,7 @@ const FormField = ({
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <View className="space-y-2">
+    <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
       <View className="w-full h-16 px-4 bg-black-200 border-2 rounded-2xl focus:border-secondary items-center flex-row">
         <TextInput
