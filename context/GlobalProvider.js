32228b731c7df7d1dd017f507d8c378ee5e0d04d
reetam100 <reetam.chatterjee@fortmindz.in>
2024-09-03 20:07:10 +0530

    working on authentication

---
+++ context/GlobalProvider.js
@@ -0,0 +1,13 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const GlobalProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {}, []);
+
+  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+};
