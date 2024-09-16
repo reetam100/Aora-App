adb8ca9fbc7c5ef5033d53d4478271982397edc3
reetam100 <reetam.chatterjee@fortmindz.in>
2024-09-16 12:29:45 +0530

    working on the videocard component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,19 +1,39 @@
-import { View, Text, FlatList, Image } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
+import { getAllPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const { data: posts, isLoading, refetch } = useAppwrite(getAllPosts);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // re call the videos
+    await refetch();
+    setRefreshing(false);
+  };
+
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }]}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
-        )}
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
@@ -40,10 +60,17 @@ const Home = () => {
               <Text className="text-gray-100 text-lg-font-pregular mb-3">
                 Latest Videos
               </Text>
-              <Trending />
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
             </View>
           </View>
         )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="Be the first one to upload a video"
+          />
+        )}
+        refreshControl={<RefreshControl />}
       />
     </SafeAreaView>
   );
---
+++ components/VideoCard.jsx
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const VideoCard = ({
+  video: {
+    title,
+    thumbnail,
+    video,
+    users: { username, avatar },
+  },
+}) => {
+  return (
+    <View className="flex-col items-center px-4 mb-14">
+      <Text className="text-2xl text-white">{title}</Text>
+    </View>
+  );
+};
+
+export default VideoCard;
